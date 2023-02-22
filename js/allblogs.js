16516c09d76e21e689fc9b8ddf4c937cc46bd9d2
Bryan <bryanmurasira@gmail.co>
2023-02-22 21:30:47 +0200

    feat(allblogs): add-js-to-blogs-page-and-subpages
    - ADD the js for the blogs page
    - Create new pages: article page(HTML, CSS & JS)
    - ADD the json file for the website data

---
+++ js/allblogs.js
@@ -0,0 +1,27 @@
+// javascript for allblogs.html
+const container = document.querySelector('main');
+
+const renderPosts = async() => {
+    let uri = "http://localhost:3001/posts";
+
+    const res = await fetch(uri);
+    const posts = await res.json();
+
+    let template = '';
+    posts.forEach(post => {
+        template += `
+            <div class="post">
+                <img src="${post.article_image}" >
+                <div class="description">
+                    <h1>${post.title}</h1>
+                    <p class="body">${post.body.slice(0, 200)}</p>
+                    <p class="polls">Likes: ${post.likes}    Dislikes: ${post.dislikes}</p>
+                    <a href="/blogpage.html?id=${post.id}"  class="read-more">Read more...</a>
+                </div>        
+            </div>
+        `
+    });
+    container.innerHTML = template;
+}
+
+window.addEventListener('DOMContentLoaded',() => renderPosts());
