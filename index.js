004a208d15b6c80f593a1c75acfaa3836d173370
Bryan <bryanmurasira@gmail.co>
2023-02-26 23:08:03 +0200

    update index.js 5

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const jsonServer = require('json-server');
-const server = jsonServer.create();
-const router = jsonServer.router('data/db.json');
-const middlewares = jsonServer.defaults({
-  static: './build',
-});
-
-server.use(middlewares);
-server.use('/api', router);
-
-server.listen(process.env.PORT || 3000, () => {
-  console.log(`JSON Server is running at ${process.env.PORT || 3000}`);
-});
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('data/db.json') 
+var cors = require('cors') 
+ 
+server.use(cors())
+server.use(router)
+server.listen(3000, () => {
+  console.log('JSON Server is running at 3000')
+})
