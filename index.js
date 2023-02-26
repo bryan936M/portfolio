8a0c3ff05d3929122c894b2706b368bb2205caf9
Bryan <bryanmurasira@gmail.co>
2023-02-26 23:23:04 +0200

    update index.js 7

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('data/db.json') 
-var cors = require('cors') 
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('data/db.json');
+const middlewares = jsonServer.defaults({
+  // Add CORS headers
+  noCors: false,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers':
+      'Origin, X-Requested-With, Content-Type, Accept',
+  },
+});
 
-server.use(cors())
-server.use(router)
-server.listen(3000, () => {
-  console.log('JSON Server is running at 3000')
-})
+server.use(middlewares);
+server.use(router);
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
