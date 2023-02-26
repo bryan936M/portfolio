3be4ba0f15978c9f0b2207feb1b4623ab598f394
Bryan <bryanmurasira@gmail.co>
2023-02-26 23:20:11 +0200

    update index.js 7

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server')
 const server = jsonServer.create()
 const router = jsonServer.router('data/db.json') 
 var cors = require('cors') 
- 
+
 server.use(cors())
 server.use(router)
 server.listen(3000, () => {
