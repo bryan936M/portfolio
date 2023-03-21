eabec069feadd5e8b17e5229da445c9b6ac9d2c8
Bryan <bryanmurasira@gmail.co>
2023-03-21 04:23:40 +0200

    feat(integration): login & admin_home
    - modified admin home page(logout feature)
    - added accesstoken & refreshtoken functionality
    - added toastify

--- js/adminhome.js
+++ js/adminhome.js
@@ -3,11 +3,13 @@
 const deleteBtn = document.querySelector('.button');
 const container = document.querySelector('.blogs-section');
 const renderPosts = async() => {
+    //ACCESS & PULL DATA FROM REST API
     let uri = ' https://cute-pear-sheep-slip.cyclic.app/posts/';
     const res = await fetch(uri);
     const posts = await res.json();
     console.log(posts);
 
+    //CREATE & DISPLAY TEMPLATE CONTAINING DATA
     let template = '';
     posts.forEach(post => {
         template += `
@@ -21,23 +23,58 @@ const renderPosts = async() => {
             </div>
         `
     })
-
     container.innerHTML = template;
 
+    
+    //ACCESS ALL BUTTONS & LISTEN FOR CLICK
     const deleteBtns = document.querySelectorAll('.button');
     deleteBtns.forEach(button =>{
         button.addEventListener('click', async(e) => {
+            //ACCESS THE CLICKED BTN THRU ITS DATASET ID
             const id = e.target.dataset.id;
-            console.log("Click by button " + id);
 
+            //SEND DELETE REQUEST WITH ID IN PARAMS
             const res = await fetch(' https://cute-pear-sheep-slip.cyclic.app/posts/' + id, {
-                method: 'Delete'
+                method: 'Delete',
+                credentials: 'include'
             });
+            Toastify({
+                text: "Deleted",
+                duration: 3000, // Time in milliseconds
+                backgroundColor: "background: linear-gradient(to bottom, #FF355E, #FF355E 50%, #FF0000 50%)"
+            }).showToast();
+            location.reload();
         });
     });
     
-
     
+    //LISTEN TO LOGOUT LINK 
+    const logoutBtn = document.querySelector('.logout');
+    logoutBtn.addEventListener('click', async (e) => {
+        //SEND LOGOUT REQUEST TO API
+        const res = await fetch('https://cute-pear-sheep-slip.cyclic.app/logout',{
+            method: 'GET',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        const result = res.json();
+        if( result.message){
+            Toastify({
+                text: result.message,
+                duration: 3000, // Time in milliseconds
+                backgroundColor: "background: linear-gradient(to bottom, #ffffff 0%, #f5f5f5 50%, #dbdbdb 100%)"
+            }).showToast();
+        } else {
+            //DELETE ACCESSTOKEN & REFRESHTOKEN COOKIE
+            localStorage.removeItem("accessToken");
+            document.cookie = "refreshToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+            //GO TO LOGIN PAGE
+            location.href = "/login.html";
+        }
+    });
 
 }
 
--- js/login.js
+++ js/login.js
@@ -1,8 +1,8 @@
 //ACCESS THE FORM ELEMENT
 const form = document.querySelector('form');
 
-
-const verify = async(e) =>{
+//LISTEN FOR THE SUBMIT EVENT IN THE FORM AND ...
+form.addEventListener('submit', async(e) =>{
     e.preventDefault();
 
     //SAVE THE LOGIN INFO
@@ -20,16 +20,33 @@ const verify = async(e) =>{
         headers: {
             'Content-Type': 'application/json'
         }
+    }).catch((error) => {
+        Toastify({
+            text: error.message,
+            duration: 3000, // Time in milliseconds
+            backgroundColor: "linear-gradient(to right, #ffcccc, #ff6666)"
+        }).showToast();
     });
 
     //RECEIVE THE ACCESS TOKEN || ERROR MESSAGES
     const result = await res.json();
-    console.log(result);
 
     //SAVE THE ACCESS TOKEN IN MEMORY
-    //REDIRECT TO ADMIN HOME PAGE
-};
-
-
-//LISTEN FOR THE SUBMIT EVENT IN THE FORM
-form.addEventListener('submit', verify);
+    if(result.accessToken){
+        localStorage.setItem("accessToken", result.accessToken);
+        //REDIRECT TO ADMIN HOME PAGE
+        Toastify({
+            text: "LOGGED IN!",
+            duration: 3000, // Time in milliseconds
+            backgroundColor: "linear-gradient(to bottom, #00c853, #00e676)"
+        }).showToast();
+        location.href = "/admin_home.html";
+    }else if ( result.message){
+        Toastify({
+            text: result.message,
+            duration: 3000, // Time in milliseconds
+            backgroundColor: "linear-gradient(to right, #ffcccc, #ff6666)"
+        }).showToast();
+    }
+    
+});
